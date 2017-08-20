53ac3d93028c50f3553e13e9940770a1a99c26b5
Friedem <friede80@gmail.com>
2017-08-20 16:44:14 -0400

    Update case of percent encoding

--- ghc-mod-core/src/GhcMod/ModuleLoader.hs
+++ ghc-mod-core/src/GhcMod/ModuleLoader.hs
@@ -227,7 +227,7 @@ uriToFilePath (FileUri uri)
 
 filePathToUri :: FilePath -> FileUri
 filePathToUri (drive:':':rest) =
-  Uri $ T.pack $ concat ["file:///", [toLower drive], "%3a", fmap convertDelim rest]
+  Uri $ T.pack $ concat ["file:///", [toLower drive], "%3A", fmap convertDelim rest]
   where
     convertDelim '\\' = '/'
     convertDelim c = c
